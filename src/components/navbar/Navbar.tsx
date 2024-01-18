47ea1aeec74a6368b5ececa6259ee6e2162bd066
Vitor Ferreira <vitornasc673@gmail.com>
2024-01-18 17:04:22 -0300

    Implementação da context, login e cadastro

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,17 +1,30 @@
-import { Link } from "react-router-dom"
+import { useContext } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { AuthContext } from "../../contexts/AuthContext";
 
 function Navbar() {
+
+    const navigate = useNavigate();
+
+    const { handleLogout } = useContext(AuthContext)
+
+    function logout() {
+        handleLogout()
+        alert('Usuário desconectado com sucesso')
+        navigate('/login')
+    }
+
     return (
         <nav className="bg-violet-700 flex justify-center text-white py-3 border-b-2">
             <div className="container flex justify-between gap-4">
                 <Link to='/home' className="font-bold text-2xl">Blog Pessoal</Link>
-                
+
                 <div className="flex gap-4 font-semibold">
                     <div className="hover:underline cursor-pointer">Postagens</div>
                     <div className="hover:underline cursor-pointer">Temas</div>
                     <div className="hover:underline cursor-pointer">Cadastrar Tema</div>
                     <div className="hover:underline cursor-pointer">Perfil</div>
-                    <div className="hover:underline cursor-pointer">Sair</div>
+                    <Link to='' onClick={logout} className="hover:underline cursor-pointer">Sair</Link>
                 </div>
             </div>
         </nav >
--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,20 +1,73 @@
-import { Link } from "react-router-dom"
+import { ChangeEvent, useContext, useEffect, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { AuthContext } from "../../contexts/AuthContext";
+import UsuarioLogin from "../../models/UsuarioLogin";
+import { RotatingLines } from "react-loader-spinner";
 
 function Login() {
+
+    const navigate = useNavigate();
+
+    const { usuario, handleLogin, isLoading } = useContext(AuthContext)
+
+    const [usuarioLogin, setUsuarioLogin] = useState<UsuarioLogin>(
+        {} as UsuarioLogin
+    )
+
+    useEffect(() => {
+        if (usuario.token !== '') {
+            navigate('/home')
+        }
+    }, [usuario])
+
+    function atualizarEstado(e: ChangeEvent<HTMLInputElement>) {
+        setUsuarioLogin({
+            ...usuarioLogin,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    function login(e: ChangeEvent<HTMLFormElement>) {
+        e.preventDefault()
+        handleLogin(usuarioLogin)
+    }
+
     return (
         <div className="text-white bg-gray-700 grid grid-cols-2 place-items-center h-screen font-semibold">
-            <form className="flex flex-col items-center justify-center gap-4 w-1/3" action="">
+
+            <form className="flex flex-col items-center justify-center gap-4 w-1/3" onSubmit={login}>
                 <h1 className="text-5xl">Logar</h1>
                 <div className="flex flex-col w-full">
-                    <label>Usuário</label>
-                    <input className="border-2 border-solid p-1 hover:border-gray-400" placeholder="Usuário" type="text" />
+                    <label htmlFor="usuario">Usuário</label>
+                    <input className="border-2 border-solid p-1 hover:border-gray-400 text-black"
+                        id="usuario"
+                        name="usuario"
+                        placeholder="Usuário"
+                        type="text"
+                        value={usuarioLogin.nome}
+                        onChange={(e: ChangeEvent<HTMLInputElement>) => atualizarEstado(e)} />
                 </div>
                 <div className="flex flex-col w-full">
-                    <label>Senha</label>
-                    <input className="border-2 border-solid p-1 hover:border-gray-400" placeholder="Senha" type="text" />
+                    <label htmlFor="senha">Senha</label>
+                    <input className="border-2 border-solid p-1 hover:border-gray-400 text-black"
+                        id="senha"
+                        name="senha"
+                        placeholder="Senha"
+                        type="password"
+                        value={usuarioLogin.senha}
+                        onChange={(e: ChangeEvent<HTMLInputElement>) => atualizarEstado(e)} />
                 </div>
-                <button className="text-white p-1 bg-gray-600 border-solid border-2 border-white hover:bg-violet-600 w-1/2 rounded text-lg">
-                    Entrar
+                <button className="flex justify-center text-white p-1 bg-gray-600 border-solid border-2 border-white hover:bg-violet-600 w-1/2 rounded text-lg"
+                    type="submit">
+                    {isLoading ? <RotatingLines
+                        strokeColor="white"
+                        strokeWidth="5"
+                        animationDuration="0.75"
+                        width="24"
+                        visible={true}
+                    /> :
+                        <span>Entrar</span>
+                    }
                 </button>
                 <p className="py-5 border-t-2 border-y-gray-200">Ainda não tem uma conta? <Link to='/cadastro' className="text-violet-500 hover:underline">Cadastre-se</Link></p>
             </form>
