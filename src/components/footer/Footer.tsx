4048b8f84d520334a145c1da21a7e2ce8bc12711
Vitor Ferreira <vitornasc673@gmail.com>
2024-01-31 15:22:42 -0300

    Update Deploy

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,6 +1,7 @@
 import { FacebookLogo, GithubLogo, LinkedinLogo } from "@phosphor-icons/react"
 import { AuthContext } from "../../contexts/AuthContext"
 import { ReactNode, useContext } from "react"
+import { Link } from "react-router-dom"
 
 function Footer() {
 
@@ -17,9 +18,9 @@ function Footer() {
                 <p className="py-1 font-bold">Blog Pessoal Generation | Copyright: {data} </p>
                 <p>Acesse nossas redes sociais</p>
                 <div className="flex gap-2 py-2">
-                    <LinkedinLogo size={44} weight="bold"></LinkedinLogo>
-                    <GithubLogo size={44} weight="bold"></GithubLogo>
-                    <FacebookLogo size={44} weight="bold"></FacebookLogo>
+                    <Link to={`https://www.linkedin.com/in/vitor-do-nascimento-ferreira/`}><LinkedinLogo size={44} weight="bold"></LinkedinLogo></Link>
+                    <Link to={`https://github.com/Vitornasc3`}><GithubLogo size={44} weight="bold"></GithubLogo></Link>
+                    <Link to={`https://www.facebook.com/vitornasc3`}><FacebookLogo size={44} weight="bold"></FacebookLogo> </Link>
                 </div>
             </div>
         )
